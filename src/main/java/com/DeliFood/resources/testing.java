8afc237a54c671ed17390f590d32f5deca9fa8ac
Piseth <piseth.ngorn@gmail.com>
2016-10-31 12:27:32 -0700

    added testing file

---
+++ src/main/java/com/DeliFood/resources/testing.java
@@ -0,0 +1,10 @@
+package com.DeliFood.resources;
+
+/**
+ * Created by Piseth on 10/31/16.
+ */
+public class testing {
+    public void main(String argu) {
+        System.out.println("Hello");
+    }
+}
